afc1157a6057f4a51f726ec8d78925f9d56cb4b7
K. M. Azwad Hossain <azwadkm22@gmail.com>
2023-11-04 03:25:12 +0600

    Update 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,26 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import BaseMap from './components/BaseMap';
 import RankingsPage from './pages/RankingsPage';
-<<<<<<< HEAD
-import What from './components/What';
-
-=======
 import WhatW from './components/WhatW';
 import What from './components/What'
->>>>>>> 8ed64419a7e63913c3f801217eac96477e0ef2f9
 function App() {
   return (
     <div className="App">
 
       {/* <RankingsPage /> */}
       {/* <BaseMap /> */}
-<<<<<<< HEAD
 
       <What />
-=======
-      <What/>
       {/* <WhatW /> */}
->>>>>>> 8ed64419a7e63913c3f801217eac96477e0ef2f9
       {/* <header className="App-header"> */}
         {/* <img src={logo} className="App-logo" alt="logo" />
         <p>

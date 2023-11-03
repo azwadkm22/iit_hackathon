d13bcd45d4880ee1d48f22deefaaf6b71425c69e
Al_RADI <alradi9923@gmail.com>
2023-11-03 13:28:15 +0600

    added some default logic

---
+++ backend/controllers/countries.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+export const getCountries = async(req,res)=>{
+    try {
+        const countries = axios.get('https://jsonplaceholder.typicode.com/users').then(
+                (response)=>{
+                    console.log(response);
+                }
+            ).catch(
+                (function(error){
+                    console.log(error); 
+                })
+            ).finally(
+                function(){
+                    console.log('this will always run')
+                }
+            );
+        res.status(200).json(counties.data);
+    } catch (error) {
+        res.status(404).json({message:error.message});
+    }
+}
--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,15 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import countryRoutes from './routes/countries.js';
+
+
 const app = express();
 app.use(bodyParser.json({limit:"30mb",extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
 app.use(cors());
+
+app.use('/countries',countryRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://hack_the_verse_ard:GkaBOED4BoayvHFX@cluster0.zd1azte.mongodb.net/';
 //GkaBOED4BoayvHFX

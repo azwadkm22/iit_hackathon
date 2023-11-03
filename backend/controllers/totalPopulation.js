5a6150e2d3a6594a74194d9db7e45aecf069bad6
KaziShadmanSakib <shadmansakib.24csedu.010@gmail.com>
2023-11-03 14:20:00 +0600

    social economic factors apis integrated

--- backend/controllers/totalPopulation.js
+++ backend/controllers/totalPopulation.js
@@ -1,1 +1,20 @@
-import mongoose from 'mongoose';
+import fetch from 'node-fetch';
+
+const country = 'BD';
+const apiUrl = 'https://api.worldbank.org/v2/country/'+ country +'/indicator/SP.POP.TOTL?format=json';
+
+const fetchData = async () => {
+  try {
+    const response = await fetch(apiUrl);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+    } else {
+      console.error('Failed to fetch data:', response.statusText);
+    }
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
+
+fetchData();

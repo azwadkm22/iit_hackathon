8c7c3f2000560059ebc8bce4fe940a91e989186c
KaziShadmanSakib <shadmansakib.24csedu.010@gmail.com>
2023-11-03 15:55:10 +0600

    all socio economic factors data parsed from APIs

--- backend/controllers/totalPopulation.js
+++ backend/controllers/totalPopulation.js
@@ -1,20 +1,45 @@
 import fetch from 'node-fetch';
 
 const country = 'BD';
-const apiUrl = 'https://api.worldbank.org/v2/country/'+ country +'/indicator/SP.POP.TOTL?format=json';
+const apiUrl = 'https://api.worldbank.org/v2/country/' + country + '/indicator/SP.POP.TOTL?format=json';
 
 const fetchData = async () => {
   try {
     const response = await fetch(apiUrl);
     if (response.ok) {
       const data = await response.json();
-      console.log(data);
+      return data;
     } else {
       console.error('Failed to fetch data:', response.statusText);
+      return null; // Return a default value or handle the error accordingly
     }
   } catch (error) {
     console.error('Error fetching data:', error);
+    return null; // Return a default value or handle the error accordingly
   }
 };
 
-fetchData();
+const scrapeData = async () => {
+  const data = await fetchData();
+  const extractedData = [];
+
+  if (data) {
+    data[1].forEach(entry => {
+      const extractedEntry = {
+        date: entry.date,
+        value: entry.value
+      };
+      extractedData.push(extractedEntry);
+    });
+  }
+
+  return extractedData;
+};
+
+// Using async/await to handle asynchronous behavior
+const getDataAndLog = async () => {
+  const extractedData = await scrapeData();
+  console.log(extractedData);
+};
+
+getDataAndLog();
